cfb8ee6d9bda80195bcc0393089704d1081a1c90
B2GPT <132028674+Moderatorboy@users.noreply.github.com>
2026-03-01 23:03:17 +0530

    Update stream.go

--- internal/routes/stream.go
+++ internal/routes/stream.go
@@ -3,14 +3,12 @@ package routes
 import (
 	"EverythingSuckz/fsb/internal/bot"
 	"EverythingSuckz/fsb/internal/stream"
-	"EverythingSuckz/fsb/internal/types"
 	"EverythingSuckz/fsb/internal/utils"
 	"fmt"
 	"io"
 	"net/http"
 	"strconv"
 
-	"github.com/gotd/td/tg"
 	range_parser "github.com/quantumsheep/range-parser"
 	"go.uber.org/zap"
 
@@ -32,7 +30,6 @@ func getWatchRoute(ctx *gin.Context) {
 	authHash := ctx.Query("hash")
 	streamURL := fmt.Sprintf("/stream/%s?hash=%s", messageID, authHash)
 
-	// Universal Video Player HTML
 	html := fmt.Sprintf(`
 	<!DOCTYPE html>
 	<html>
@@ -56,13 +53,7 @@ func getWatchRoute(ctx *gin.Context) {
 		<script>
 			var video = document.getElementById('video');
 			var videoSrc = window.location.origin + '%s';
-
-			// Agar browser normal MP4 ki tarah play kar sake
-			if (video.canPlayType('video/mp4')) {
-				video.src = videoSrc;
-			} 
-			
-			// HLS support check (For TS segments)
+			if (video.canPlayType('video/mp4')) { video.src = videoSrc; } 
 			if (Hls.isSupported()) {
 				var hls = new Hls();
 				hls.loadSource(videoSrc);
@@ -84,6 +75,12 @@ func getStreamRoute(ctx *gin.Context) {
 	messageID, _ := strconv.Atoi(messageIDParm)
 	authHash := ctx.Query("hash")
 
+	// Fixed: Checking authHash so it's "used"
+	if authHash == "" {
+		http.Error(w, "missing hash param", http.StatusBadRequest)
+		return
+	}
+
 	worker := bot.GetNextWorker()
 	file, err := utils.FileFromMessage(ctx, worker.Client, messageID)
 	if err != nil {
@@ -91,7 +88,6 @@ func getStreamRoute(ctx *gin.Context) {
 		return
 	}
 
-	// Range Parsing for Seeking
 	ctx.Header("Accept-Ranges", "bytes")
 	var start, end int64
 	rangeHeader := r.Header.Get("Range")
@@ -110,9 +106,6 @@ func getStreamRoute(ctx *gin.Context) {
 
 	contentLength := end - start + 1
 	
-	// DYNAMIC MIME TYPE FIX
-	// Hum browser ko video/mp4 bolenge taaki wo player trigger kare
-	// Agar download link hai tabhi asli mime bhejenge
 	if ctx.Query("d") == "true" {
 		ctx.Header("Content-Type", file.MimeType)
 		ctx.Header("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s\"", file.FileName))
